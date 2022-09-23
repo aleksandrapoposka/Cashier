94600ad7e87108523f292d2d523d0d0681bd1059
Aleksandra Poposka <aleksandra.poposka@btobet.com>
2022-09-23 19:29:15 +0200

    Add project files.

---
+++ Cashier/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities.User;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cashier.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
