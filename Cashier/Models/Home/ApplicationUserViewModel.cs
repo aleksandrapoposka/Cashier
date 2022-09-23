5d58dfa358714a0ae66372ecf2d3d7ce332d11e4
Aleksandra Poposka <aleksandra.poposka@btobet.com>
2022-09-23 19:44:58 +0200

    add user view model & validations

---
+++ Cashier/Models/Home/ApplicationUserViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cashier.Models.Home
+{
+    public class ApplicationUserViewModel
+    {
+        [Required]
+        [MaxLength(200)]
+        [MinLength(2)]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+        
+        [Required]
+        [DisplayName("Second name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayName("Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords don't match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
