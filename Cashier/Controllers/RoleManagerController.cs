bea8e9a53a5340c71ad1550c842a9dcc09ce8905
Aleksandra Poposka <aleksandra.poposka@btobet.com>
2022-09-26 20:19:49 +0200

    add new role functionality

--- Cashier/Controllers/RoleManagerController.cs
+++ Cashier/Controllers/RoleManagerController.cs
@@ -1,14 +1,57 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace Cashier.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "SuperAdmin")]
     public class RoleManagerController : Controller
     {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public RoleManagerController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(string roleName)
+        {
+            var roles = _roleManager.Roles.ToList();
+
+            if (string.IsNullOrEmpty(roleName))
+            {
+                return View("Index", roles);
+            }
+
+            //validate if role exists
+            if (roles.Any(x => x.Name.ToLower() == roleName.ToLower()))
+            {
+                return View("Index", roles);
+            }
+
+            var result = await _roleManager.CreateAsync(new IdentityRole
+            {
+                Name = roleName,
+                NormalizedName = roleName.ToUpper()
+            });
+            if (result.Succeeded)
+            {
+                roles = _roleManager.Roles.ToList();
+                return View("Index", roles);
+            }
+            else
+            {
+                //add error handling
+            }
+
+            return View("Index", roles);
         }
 
     }
