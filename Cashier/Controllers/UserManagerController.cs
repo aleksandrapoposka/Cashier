34614c694a2d9babfdcec86a5f415873a53fe806
Aleksandra Poposka <aleksandra.poposka@btobet.com>
2022-09-26 20:31:33 +0200

    add user manager

---
+++ Cashier/Controllers/UserManagerController.cs
@@ -0,0 +1,28 @@
+﻿using Entities.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cashier.Controllers
+{
+    [Authorize(Roles = "SuperAdmin")]
+    public class UserManagerController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public UserManagerController(
+            UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = _userManager.Users.ToList();
+            var roles = _roleManager.Roles.ToList();
+            return View();
+        }
+    }
+}
