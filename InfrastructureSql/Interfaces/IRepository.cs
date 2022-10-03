2d9d0075e63e36014ff075412c2a1c8591d1bf44
Aleksandra Poposka <aleksandra.poposka@btobet.com>
2022-10-03 18:46:53 +0200

    add base repositories

---
+++ InfrastructureSql/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureSql.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> GetById(long id);
+        Task<List<T>> GetAll();
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task Delete(T entity);
+    }
+}
