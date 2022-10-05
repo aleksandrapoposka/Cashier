bd6112d87d6cd428f33f28d9c8b9a891c3fae7af
BTOBET\f.cvetkovski <f.cvetkovski@btobet.com>
2022-10-05 18:29:38 +0200

    Mongo class 07_1

---
+++ Entities/BaseMongoEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+namespace Entities
+{
+    public class BaseMongoEntity
+    {
+        [Serializable]
+        public abstract class MongoBaseEntity
+        {
+            [BsonId]
+            public ObjectId Id { get; set; }
+            public string ModifiedBy { get; set; }
+            public DateTime ModifiedOn { get; set; }
+        }
+    }
+}
---
+++ InfrastructureMongoDB/Concrete/ArticleImageRepository.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess;
+using Entities.Articles;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace InfrastructureMongoDB
+{
+    public class ArticleImageRepository : IArticleImageRepository
+    {
+        private readonly IMongoCollection<ArticleImage> _images;
+
+        public ArticleImageRepository(IMongoDBConnection settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _images = database.GetCollection<ArticleImage>(settings.MongoDBCollectionName);
+        }
+
+        public ArticleImage Create(ArticleImage image)
+        {
+            _images.InsertOne(image);
+            return image;
+        }
+
+        public List<ArticleImage> Get()
+        {
+            return _images.Find(image => true).ToList();
+        }
+
+        public ArticleImage Get(long articleId)
+        {
+            return _images.Find(image => image.ArticleId == articleId).FirstOrDefault();
+        }
+
+        public void Remove(long articleId)
+        {
+            _images.DeleteOne(image => image.ArticleId == articleId);
+
+        }
+
+        public void Update(long articleId, ArticleImage image)
+        {
+            _images.ReplaceOne(image => image.ArticleId == articleId, image);
+        }
+    }
+}
